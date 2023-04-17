b00c1ca5c12e25fda6ef6b81f0a81cd3a557cf3a
arvindshokhanda <arvindshokhanda21@gmail.com>
2023-04-17 18:06:13 +0530

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,24 @@
+import Navbar from './Navbar';
+import Home from './Home'
+import AddProduct from './AddProduct';
+import ProductDetail from './ProductDetail';
+import Cart from './Cart';
+import { Route, Routes } from 'react-router-dom';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+      <Route path = "/" element={<Home />} />
+      <Route path="ProductDetail/:id" element={<ProductDetail />} />
+      <Route path="AddProduct" element={<AddProduct />} />
+      <Route path="Cart" element={<Cart />} />
+
+
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
