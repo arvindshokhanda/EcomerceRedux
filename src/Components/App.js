d419e50b4c62ad5713399d127175d73aaeb5a2c2
arvindshokhanda <arvindshokhanda21@gmail.com>
2023-04-18 10:32:33 +0530

    again a try

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,7 +10,7 @@ function App() {
       <Navbar/>
     
       <Routes>
-      <Route path = "/" index  element={<Home />}/>
+      <Route path = "/"  element={<Home />}/>
       <Route path="ProductDetail/:id" element={<ProductDetail />} />
       <Route path="AddProduct" element={<AddProduct />} />
       <Route path="Cart" element={<Cart />} />
